48a0fefbd2654f58d3a2b8bf4103938e4826c75e
Siddarth Kudligi <siddarth171999@gmail.com>
2021-09-07 19:17:26 +0530

    added careers backbone

---
+++ src/_data/solutions/gwas.js
@@ -0,0 +1,54 @@
+module.exports = {
+  solutionPoints: [
+    //   "Aganitha Omics Kube (AOK) on AWS/GCP/Azure/HPC",
+    //   "Open-source state-of-art platforms for Omics pipelines from Broad Institute",
+    //   "Integration with Illumina BaseSpace, PacBio SMRT Link, 10x Genomics Cell Ranger, Trans-Proteomic Pipeline from Institute for Systems Biology (Seattle)",
+    //   "APIs for integration with ELNs such as Benchling",
+  ],
+  solutionHighlights: [
+    {
+      title: "Scalable processing power",
+      description:
+        "Proven to scale well for processing vast data sets such as UK BioBank 500k WES dataset",
+      background: "bg-red-100",
+    },
+    {
+      title: "Cross-function compatible",
+      description:
+        "Vertical integration and optimization, providing best-in-class performance",
+    },
+    {
+      title: "Modifiable-off-the-shelf (MOTS) platform",
+      description:
+        "Pre-packaged service offering (PSO) for platform rollout along-side a full catalog of service offerings",
+    },
+    {
+      title: "Open-source tech & API integration",
+      description:
+        "Based on best-in-class open-source technologies and has APIs for integration with ELNs",
+    },
+    {
+      title: "Cloud & Scheduler compatible",
+      description:
+        "Compatible with cloud providers supporting Kubernetes, and classic HPC schedulers",
+    },
+    {
+      title: "Integrated with sequencing solutions",
+      description:
+        "Integrated with Illumina BaseSpace, PacBio SMRT Link, 10x Genomics Cell Ranger, Trans-Proteomic Pipeline from Institute for Systems Biology (Seattle)",
+    },
+  ],
+  benefits: [
+    "Understand Mendelian violations in trios, prune variants in linkage disequilibrium, analyze genetic similarity between samples, and compute sample scores and variant loadings using PCA",
+    "Perform variant, gene-burden and eQTL association analyses using linear, logistic, and linear mixed regression, and estimate heritability",
+    "Reusability of tools saving time to rewrite from scratch and regeneratable results for future",
+    "Ability to use scalable cloud computing without learning about nitty gritty details",
+    "Caching of runs saving computational cost",
+  ],
+  contents: [
+    "Introduction",
+    "Our Solution",
+    "Solution Highlights",
+    "Benefits / Outcomes",
+  ],
+};
