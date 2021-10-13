69199313ef22bec659c05eeba9289b5b2faa4fb9
Siddarth Kudligi <siddarth171999@gmail.com>
2021-10-13 20:46:40 +0530

    changed the context section on the homepage, added icons consistent with our color scheme

--- src/_data/contextPoints.js
+++ src/_data/contextPoints.js
@@ -1,33 +1,33 @@
 module.exports = [
   {
     title: `Contextualized <span class="italic">in silico</span> models`,
-    description: `Enable a virtuous round-tripping cycle between in silico and <span class="italic">in vitro</span> experiments by developing customer specific models blending advances in biology, chemistry, and computing.`,
-    icon: "/assets/images/homepage/computer.png",
+    description: `Enable a virtuous round-tripping cycle between <span class="italic">in silico</span> and <span class="italic">in vitro</span> experiments by developing customer specific models blending advances in biology, chemistry, and computing.`,
+    icon: "/assets/images/homepage/silico-1.svg",
   },
   {
     title: "High throughput automation",
     description: `Provide quick insights by intertwining analytics and AI in conjunction with high throughput automation helping reduce cycle times and drive efficiencies.`,
-    icon: "/assets/images/homepage/automation.png",
+    icon: "/assets/images/homepage/hp.svg",
   },
   {
     title: "Data as an R&D Asset",
     description: `Organize and enrich enterprise data in an easy to consume format for scientists helping them increase research productivity and speed of strategic decision making.`,
-    icon: "/assets/images/homepage/data.png",
+    icon: "/assets/images/homepage/data.svg",
   },
 
   {
     title: `High concentration biological design`,
     description: `Characterize high concentration samples to maximize the efficiency of screening, while minimizing the total amount of samples required for analysis using <span class="italic">in silico</span> models.`,
-    icon: "/assets/images/homepage/biology.png",
+    icon: "/assets/images/homepage/bio.svg",
   },
   {
     title: `Quantum chemical descriptors`,
     description: `Predict low yielding reactions and enable accurate predictions of activation energy with quantum chemical descriptors with machine learning while decreasing avoidable experiment expenditure.`,
-    icon: "/assets/images/homepage/chemical-reaction.png",
+    icon: "/assets/images/homepage/chemical.svg",
   },
   {
     title: `R&D platform tools in a box`,
     description: `Simplify setting up the <span class="italic">in silico</span> computing platform and artificial intelligence building blocks that complement the traditional lab-based approaches across drug research and development activities.`,
-    icon: "/assets/images/homepage/toolbox.png",
+    icon: "/assets/images/homepage/tools.svg",
   },
 ];
