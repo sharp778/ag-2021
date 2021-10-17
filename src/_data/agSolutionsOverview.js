5ea6a7b4bb6c3fa5ab66f9427e62cd966daf4659
Siddarth Kudligi <siddarth171999@gmail.com>
2021-10-18 01:15:11 +0530

    changed solutions overview page

---
+++ src/_data/agSolutionsOverview.js
@@ -0,0 +1,13 @@
+module.exports = [
+  `Organization and search of large amounts of information in data stores.`,
+  `Orchestration of flows between systems and humans.`,
+  `Orchestration of computational blocks (through technologies such as Docker and Kubernetes).`,
+
+  `Biology domain and computational/
+  <span class="italic">in silico</span>
+      models of the same.`,
+  `Chemistry domain and computational/
+    <span class="italic">in silico</span>
+      models of the same.`,
+  `AI/ML`,
+];
--- src/_data/reference.js
+++ src/_data/reference.js
@@ -3,23 +3,23 @@ module.exports = [
     title: "Computational infrastructure and foundation",
     description: `Designing, setting up, and managing large-scale high-performance computing infrastructure and automating the deployment of containerized applications.
     We provide a foundation necessary for executing data and resource-intensive AI/ML programs supporting lab assays.`,
-    icon: "/assets/images/homepage/foundation.svg",
+    icon: "/assets/images/overview/cloud-data.svg",
   },
   {
     title: "Pipelining frameworks and data management",
     description: `From identifying the composition of a protein to accelerating drug discovery from a sample with industry-standard pipeline frameworks - genomics, next-generation sequencing, proteomics data analysis by creating a knowledge management framework.
     `,
-    icon: "/assets/images/homepage/plumbing.svg",
+    icon: "/assets/images/overview/pipeline.svg",
   },
   {
     title: `Omics solution components and knowledge graph`,
     description: `Assembling the genome with specific algorithms or aligning with the reference, associating a gene with a disease, identifying if the gene is translating into a protein, recognizing the constituents of the produced protein, understanding the metabolism when a drug is taken, and building data catalogs, metadata, and knowledge graphs.`,
-    icon: "/assets/images/homepage/dna.svg",
+    icon: "/assets/images/overview/gene-therapy.svg",
   },
   {
     title: `Target bio/cheminformatics solutions and knowledge hubs
     `,
     description: `Developing a variety of client-specific target solutions leveraging the solution components developed in the previous layers.`,
-    icon: "/assets/images/homepage/molecule.svg",
+    icon: "/assets/images/homepage/chemical.svg",
   },
 ];
