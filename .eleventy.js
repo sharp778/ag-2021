f4bfdf15233543eade21cac435b06fceb047e3f4
Siddarth Kudligi <siddarth171999@gmail.com>
2021-10-22 21:58:36 +0530

    optimised images, added eleventy image plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,98 @@
 const now = String(Date.now());
 
+const Image = require("@11ty/eleventy-img");
+
+async function imageShortcode(src, alt, myClass) {
+  if (alt === undefined) {
+    // You bet we throw an error on missing alt (alt="" works okay)
+    throw new Error(`Missing \`alt\` on myImage from: ${src}`);
+  }
+
+  let metadata = await Image(src, {
+    widths: [760, 1000, 1200],
+    formats: ["webp", "jpeg"],
+  });
+
+  let data = metadata.jpeg[metadata.jpeg.length - 1];
+  return `<img src="${data.url}" class=${myClass} alt="${alt}" loading="lazy" decoding="async">`;
+}
+
+async function imageShortcodeBanner(src, alt, myClass) {
+  if (alt === undefined) {
+    // You bet we throw an error on missing alt (alt="" works okay)
+    throw new Error(`Missing \`alt\` on myImage from: ${src}`);
+  }
+
+  let metadata = await Image(src, {
+    widths: [1000, 1200, 1600, 2000],
+    formats: ["webp", "jpeg"],
+  });
+
+  let data = metadata.jpeg[metadata.jpeg.length - 1];
+  return `<img src="${data.url}" class=${myClass} alt="${alt}" loading="lazy" decoding="async">`;
+}
+
+async function imageShortcodeClickable(src, alt, myClass) {
+  if (alt === undefined) {
+    // You bet we throw an error on missing alt (alt="" works okay)
+    throw new Error(`Missing \`alt\` on myImage from: ${src}`);
+  }
+
+  let metadata = await Image(src, {
+    widths: [760, 1000, 1280],
+    formats: ["webp", "jpeg"],
+  });
+
+  let data = metadata.jpeg[metadata.jpeg.length - 1];
+  // console.log(data);
+  return `<a href=${data.url} target="_blank">
+  <img src="${data.url}" class=${myClass} alt="${alt}" loading="lazy" decoding="async">
+  </a>`;
+}
+
+async function imageShortcodePng(src, alt, myClass) {
+  if (alt === undefined) {
+    // You bet we throw an error on missing alt (alt="" works okay)
+    throw new Error(`Missing \`alt\` on myImage from: ${src}`);
+  }
+
+  let metadata = await Image(src, {
+    widths: [760, 1000, 1200],
+    formats: ["webp", "png"],
+  });
+
+  let lowestSrc = metadata["png"][0];
+
+  const srcset = Object.keys(metadata).reduce(
+    (acc, format) => ({
+      ...acc,
+      [format]: metadata[format].reduce(
+        (_acc, curr) => `${_acc} ${curr.srcset} ,`,
+        ""
+      ),
+    }),
+    {}
+  );
+
+  console.log("/////////////", srcset);
+
+  let data = metadata.png[metadata.png.length - 1];
+  console.log(data);
+  // console.log(metadata);
+  return `<a href=${data.url} target="_blank">
+  <img src="${data.url}" class="${myClass}" alt="${alt}" loading="lazy" srcset="${srcset["png"]}" decoding="async">
+  </a>`;
+}
+
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
+  eleventyConfig.addNunjucksAsyncShortcode("imagePng", imageShortcodePng);
+  eleventyConfig.addNunjucksAsyncShortcode("imageBanner", imageShortcodeBanner);
+
+  eleventyConfig.addNunjucksAsyncShortcode(
+    "imageClickable",
+    imageShortcodeClickable
+  );
   eleventyConfig.setUseGitIgnore(false);
   eleventyConfig.addWatchTarget("./_tmp/style.css");
   eleventyConfig.addPassthroughCopy({ "./_tmp/style.css": "./style.css" });
@@ -12,6 +104,7 @@ module.exports = function (eleventyConfig) {
     "./node_modules/alpinejs/dist/cdn.js": "./js/alpine.js",
   });
   eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addFilter("myFilter", function (value) {
     if (value.match(/in silico|in vitro/gi)) {
       return value.replace(/in silico|in vitro/gi);
--- src/_data/solutionsList.js
+++ src/_data/solutionsList.js
@@ -4,20 +4,20 @@ module.exports = {
       title: "Knowledge Hubs",
       link: `/biopharma/knowledge-hubs/`,
       description: `AI-powered curation of fast-evolving R&D insights and Competitive landscape.`,
-      image: "/assets/images/solutions/hubs/hubs.jpeg",
+      image: "/assets/images/solutions/hubs/hubs-intro.jpg",
     },
     {
       title: "GWAS Platform",
       link: `/biopharma/genome-wide-association-studies/`,
       description: `Interrogate biobank-scale WES datasets with a proven GWAS platform.`,
-      image: "/assets/images/gwas.jpeg",
+      image: "/assets/images/solutions/gwas/gwas-intro.jpeg",
     },
     {
       title: `Omics Pipelining`,
       link: `/biopharma/omics-pipelining`,
       description: `Rapid, scalable, affordable, managed enterprise-class 
       Omics pipelining on HPC/Cloud as a managed service.`,
-      image: "/assets/images/solutions/omics/omics.jpeg",
+      image: "/assets/images/solutions/omics/omics-intro.jpg",
     },
   ],
 
@@ -26,14 +26,14 @@ module.exports = {
       title: "GWAS Platform",
       link: `/biopharma/genome-wide-association-studies/`,
       description: `Interrogate biobank-scale WES datasets with a proven GWAS platform.`,
-      image: "/assets/images/gwas.jpeg",
+      image: "/assets/images/solutions/gwas/gwas-intro.jpeg",
     },
     {
       title: `Omics Pipelining`,
       link: `/biopharma/omics-pipelining`,
       description: `Rapid, scalable, affordable, managed enterprise-class 
       Omics pipelining on HPC/Cloud as a managed service.`,
-      image: "/assets/images/solutions/omics/omics.jpeg",
+      image: "/assets/images/solutions/omics/omics-intro.jpg",
     },
   ],
 
@@ -42,14 +42,14 @@ module.exports = {
       title: "Knowledge Hubs",
       link: `/biopharma/knowledge-hubs/`,
       description: `AI-powered curation of fast-evolving R&D insights and Competitive landscape.`,
-      image: "/assets/images/solutions/hubs/hubs.jpeg",
+      image: "/assets/images/solutions/hubs/hubs-intro.jpg",
     },
     {
       title: `Omics Pipelining`,
       link: `/biopharma/omics-pipelining`,
       description: `Rapid, scalable, affordable, managed enterprise-class 
       Omics pipelining on HPC/Cloud as a managed service.`,
-      image: "/assets/images/solutions/omics/omics.jpeg",
+      image: "/assets/images/solutions/omics/omics-intro.jpg",
     },
   ],
 
@@ -58,13 +58,13 @@ module.exports = {
       title: "Knowledge Hubs",
       link: `/biopharma/knowledge-hubs/`,
       description: `AI-powered curation of fast-evolving R&D insights and Competitive landscape.`,
-      image: "/assets/images/solutions/hubs/hubs.jpeg",
+      image: "/assets/images/solutions/hubs/hubs-intro.jpg",
     },
     {
       title: "GWAS Platform",
       link: `/biopharma/genome-wide-association-studies/`,
       description: `Interrogate biobank-scale WES datasets with a proven GWAS platform.`,
-      image: "/assets/images/gwas.jpeg",
+      image: "/assets/images/solutions/gwas/gwas-intro.jpeg",
     },
   ],
 };
